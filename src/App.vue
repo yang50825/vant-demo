f9f1b8b26cfa7f0a058f8fbae5b8862b834ce3d6
Layne <fdgod5@163.com>
2022-11-04 15:38:29 +0800

    element-ui和vant使用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <van-button type="primary" @click="btn">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button> -->
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+    <div class="block">
+      <span class="demonstration">默认 Hover 指示器触发</span>
+      <el-carousel height="250px" :interval="1000" indicator-position="outside">
+        <el-carousel-item v-for="(val, index) in list" :key="index">
+          <img :src="val" style="width: 100%; height: 100%" />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { Dialog } from 'vant'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      list: [
+        'https://img1.baidu.com/it/u=133990705,310893509&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1667667600&t=a29e7543c3f08fa9b38d6931b7529f95',
+        'https://img1.baidu.com/it/u=3009731526,373851691&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1667667600&t=bfd1723cd32d57c0ed2cd8f81b61f611',
+        'https://img0.baidu.com/it/u=2028084904,3939052004&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1667667600&t=fe770fc4add8ab676decf212d3756ede',
+        'https://img1.baidu.com/it/u=3622442929,3246643478&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1667667600&t=0d55483a1ce8281bcc4c5a2b05ba66eb',
+        'https://t9.baidu.com/it/u=902946645,2320843792&fm=218&app=126&size=f242,150&n=0&f=JPEG&fmt=auto?s=5EA49544FE03134D3E8DED820300708B&sec=1667667600&t=245ba5000b811aa3bd3cf3343409712a',
+      ],
+    }
+  },
+  methods: {
+    // btn() {
+    //   alert(1)
+    //   Dialog({ message: '提示', showCancelButton: true })
+    // },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.block {
+  width: 500px;
+}
+
+.block .el-carousel__button {
+  border-radius: 50%;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+
+.el-carousel-item {
+  border-radius: 50%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+
+// Vue.use(Vant)
+// import { Button } from 'vant'
+// Vue.use(Button)
+import { Button, Carousel, CarouselItem } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
